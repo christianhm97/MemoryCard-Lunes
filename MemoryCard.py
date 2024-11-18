d71c25cb31658eeef33c4eb4ba45c1ac0e044b2c
christianhm97 <77364211+christianhm97@users.noreply.github.com>
2024-11-18 19:35:53 +0100

    Update MemoryCard.py

--- MemoryCard.py
+++ MemoryCard.py
@@ -1,54 +1,79 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-
-aplicacion = QApplication([])
-ventana = QWidget()
-ventana.show()
-ventana.setWindowTitle('Memory Card')
-
-lb_Question = QLabel('Aquí veremos la pregunta')
-btn_Ok = QPushButton('Responder')
-
-RadioGroupBox = QGroupBox('Opciones de respuesta')
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel)
+ 
+ 
+app = QApplication([])
+ 
+ 
+window = QWidget()
+window.setWindowTitle('Tarjeta de memoria')
+ 
+ 
+'''Interfaz para la aplicación de Tarjeta de memoria'''
+btn_OK = QPushButton('Responder') # botón de responder
+lb_Question = QLabel('Aquí irá nuestra pregunta') # texto de pregunta
+ 
+ 
+RadioGroupBox = QGroupBox("Opciones de respuesta") # grupo en la pantalla para botones de radio con respuestas
+RadioGroupBox.hide()
 rbtn_1 = QRadioButton('Respuesta 1')
 rbtn_2 = QRadioButton('Respuesta 2')
 rbtn_3 = QRadioButton('Respuesta 3')
 rbtn_4 = QRadioButton('Respuesta 4')
-
-layout_ans1 = QHBoxLayout()
-layout_ans2 = QVBoxLayout()
+ 
+AnsGroupBox = QGroupBox("Resultado de prueba")
+lb_Result = QLabel("Correcto-Incorrecto")
+lb_Correct = QLabel("Respuesta correcta")
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment = (Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment= Qt.AlignHCenter, stretch = 2)
+AnsGroupBox.setLayout(layout_res)
+ 
+ 
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() # los verticales estarán dentro de los horizontales
 layout_ans3 = QVBoxLayout()
-
-layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_1) # dos respuestas en la primera columna
 layout_ans2.addWidget(rbtn_2)
-layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_3) # dos respuestas en la segunda columna
 layout_ans3.addWidget(rbtn_4)
-
+ 
+ 
 layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3)
-
-RadioGroupBox.setLayout(layout_ans1)
-
-layout_line1 = QHBoxLayout()
-layout_line2 = QHBoxLayout()
-layout_line3 = QHBoxLayout()
-
-layout_line1.addWidget(lb_Question, alignment = (Qt.AlignHCenter | Qt.AlignVCenter))
+layout_ans1.addLayout(layout_ans3) # las columnas están en la misma línea
+ 
+ 
+RadioGroupBox.setLayout(layout_ans1) # el “panel” con opciones de respuesta está listo 
+ 
+ 
+layout_line1 = QHBoxLayout() # pregunta
+layout_line2 = QHBoxLayout() # opciones de respuesta o resultados de prueba
+layout_line3 = QHBoxLayout() # botón de “Responder”
+ 
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
 layout_line2.addWidget(RadioGroupBox)
+layout_line2.addWidget(AnsGroupBox)
+ 
+ 
 layout_line3.addStretch(1)
-layout_line3.addWidget(btn_Ok, stretch = 2)
+layout_line3.addWidget(btn_OK, stretch=2) # el botón debería ser grande
 layout_line3.addStretch(1)
-
+ 
+ 
+# Ahora vamos a colocar las líneas que hemos creado una debajo de la otra:
 layout_card = QVBoxLayout()
-
-layout_card.addLayout(layout_line1, stretch = 2)
-layout_card.addLayout(layout_line2, stretch = 8)
+ 
+ 
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
 layout_card.addStretch(1)
-layout_card.addLayout(layout_line3, stretch = 1)
-
-layout_card.setSpacing(5)
-
-ventana.setLayout(layout_card)
-
-aplicacion.exec_()
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # los espacios entre el contenido
+ 
+ 
+window.setLayout(layout_card)
+window.show()
+app.exec()
